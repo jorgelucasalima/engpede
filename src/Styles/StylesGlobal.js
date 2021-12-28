63566db57c4e8a06c48e68d1ccd83298d9e1dbf2
jorgelucasalima <jorgelucasalima@gmail.com>
2021-12-28 14:46:45 -0300

    ajuste no stylesglobal

--- src/Styles/StylesGlobal.js
+++ src/Styles/StylesGlobal.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 //meus estilos globais
-export const GlobalStyle = createGlobalStyle`
+export const EstiloGlobal = createGlobalStyle`
 
   :root {
     --cor-fundo-header: #4F46E5;
@@ -22,7 +22,37 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1088px) {
+      font-size: 93.75%; //15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+  }
 
+  body {
+    background: var(--cordefundo);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+  
+  [disabled] {
+    opacity: 8.6;
+    cursor: not-allowed;
+  }
   
 
 
