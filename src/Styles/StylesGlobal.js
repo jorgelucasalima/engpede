d23585bff3fb49cc5b501bf04089dda528762cc2
jorgelucasalima <jorgelucasalima@gmail.com>
2021-12-28 14:43:03 -0300

    inclusão das cores no styles

---
+++ src/Styles/StylesGlobal.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+//meus estilos globais
+export const GlobalStyle = createGlobalStyle`
+
+  :root {
+    --cor-fundo-header: #4F46E5;
+    --cor-fundo: #F3F4F6;
+    --cor-fundo-outros: #FFFFFF;
+
+    --cor-texto-menu: #FFFFFF;
+    --cor-icone-menu: #FFFFFF;
+
+    --cor-texto: #6B7280;
+    --cor-botao: #A5B4FC;
+
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+
+  
+
+
+`
