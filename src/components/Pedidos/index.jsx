704eee1df035c083aad283dd10422f03c58a2492
jorgelucasalima <jorgelucasalima@gmail.com>
2022-01-10 12:48:26 -0300

    inclusão do que fazer no trello

--- src/components/Pedidos/index.jsx
+++ src/components/Pedidos/index.jsx
@@ -1,14 +1,6 @@
 import { FaRegEye, FaEdit, FaRegTrashAlt } from "react-icons/fa"
 import { Container } from "./styles"
 
-//1. ajustar o style do tr e do td 
-//2. ajustar os svg dos icones
-//3. criar a collecion dos pedidos no firebase
-//4. criar a collecion dos produtos no firebase
-//5. criar a collecion dos usuarios no firebase
-//6. criar o map de pedidos no table
-//7. criar regra do botão de adicionar pedido
-//8. cria cadastro de produtos em produtos
 
 export default function Pedidos() {
 
